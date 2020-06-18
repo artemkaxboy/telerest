e59945f78a914dda0cb654db56863607bf9858fe
Artem Kolin <artemkaxboy@gmail.com>
2020-06-18 22:54:03 +0700

    Real Initial commit
    This is the first commit from local git repo. The online one was created with
    its own initial commit which includes README and LICENSE

---
+++ src/main/kotlin/com/artemkaxboy/telerest/TelerestApplication.kt
@@ -0,0 +1,11 @@
+package com.artemkaxboy.telerest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TelerestApplication
+
+fun main(args: Array<String>) {
+    runApplication<TelerestApplication>(*args)
+}
---
+++ src/test/kotlin/com/artemkaxboy/telerest/TelerestApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.artemkaxboy.telerest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TelerestApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
